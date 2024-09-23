2ee778eb2bf0e7803e4983f23cd542e0690aebca
padma-105 <padmavathiboya22@gmail.com>
2024-09-23 22:17:20 +0530

    looping statements

---
+++ loopstmt/loop.js
@@ -0,0 +1,28 @@
+//task-1
+for(var i=1;i<=10;i++){
+    console.log(i);
+}
+
+//task-2
+for(var i=1;i<=10;i++){
+    console.log("5"+"*"+i+"="+(5*i));
+}
+
+//task-2
+var x=0;
+for(var i=1;i<=10;i++){
+   x=x+i;
+  
+}
+console.log("sum of first 10 natural numbers: "+x);
+
+//task-4
+for(var i=1;i<=30;i++){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+//task-5
+for(var i=1;i<=10;i++){
+    console.log(`square${i}=${i*i}`);
+}
